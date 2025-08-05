34fae4b0377e99ec586496dbc472d9481a614b9d
anetczuk <anetczuk@o2.pl>
2025-08-05 17:18:41 +0200

    Rename directory

--- examples/generate-all.sh
+++ examples/generate-all.sh
@@ -7,8 +7,9 @@ set -eu
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 
-"$SCRIPT_DIR"/antwiki_key_queen_lasius/generate.sh
+"$SCRIPT_DIR"/key_ant_queen_lasius_wilson/generate.sh
 
 
 ## generate small images
 #$SCRIPT_DIR/generate_small.sh
+
